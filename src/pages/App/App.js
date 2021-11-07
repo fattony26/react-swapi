fa8a5cbb7e6e6ebe446df0618abacf48a06526ba
fattony26 <marco.moran26@hotmail.com>
2021-11-07 17:11:34 -0500

    getting lots of invariant and new errors from the packages folder

---
+++ src/pages/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Component } from 'react';
+// import Starships from '../Starships/Starships';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello Kitty</h1>
+      </>
+    )
+  }
+}
+
+export default App;
--- src/sevices/api-calls.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = 'https://swapi.dev/'
-
-export function getAllStarships() {
-    return fetch(`${baseUrl}/api/starships/`)
-    .then(res => res.json());
-}
