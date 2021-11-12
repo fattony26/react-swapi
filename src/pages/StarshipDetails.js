d30d219aec8f804854102f7b7cd52110a04d63e4
fattony26 <marco.moran26@hotmail.com>
2021-11-12 11:11:41 -0500

    swapi complete

--- src/pages/StarshipDetails.js
+++ src/pages/StarshipDetails.js
@@ -1,23 +1,27 @@
 import { useLocation } from 'react-router'
 import './App.css';
+import NavBar from './NavBar';
 
 
 function StarshipDetails() {
     let starship = useLocation().state.ship;
 
     return (
-        <div className='detail'>
-            <h1>{starship.name}</h1>
-            <p><span>Manufacturer: </span> {starship.manufacturer}</p>
-            <p><span>Model: </span>{starship.model}</p>
-            <p><span>Class: </span>{starship.starship_class}</p>
-            <br/>
-            <h3>Technical Specifics</h3>
-            <p><span>Length: </span>{starship.length} meters</p>
-            <p><span>Maximum Atmospheric Speed: </span>{starship.max_atmosphering_speed} kph</p>
-            <br/>
-            <a href='/'>Go Back</a>
-        </div>
+        <>
+            <NavBar />
+            <div className='detail'>
+                <h1>{starship.name}</h1>
+                <p><span>Manufacturer: </span> {starship.manufacturer}</p>
+                <p><span>Model: </span>{starship.model}</p>
+                <p><span>Class: </span>{starship.starship_class}</p>
+                <br />
+                <h3>Technical Specifics</h3>
+                <p><span>Length: </span>{starship.length} meters</p>
+                <p><span>Maximum Atmospheric Speed: </span>{starship.max_atmosphering_speed} kph</p>
+                <br />
+                <a href='/'>Go Back</a>
+            </div>
+        </>
     )
 }
 
